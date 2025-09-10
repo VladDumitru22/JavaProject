8d3d79a5aaa3a91ec3d41e09e00e87c1ad2f6bad
Dumitru Vlad-Mihai <vlad.liviu22@gmail.com>
2025-09-10 17:15:25 +0300

    Modify ClientPeer class to support Thread execution

--- ChatClient/src/com/chatapp/client/ClientPeer.java
+++ ChatClient/src/com/chatapp/client/ClientPeer.java
@@ -1,29 +1,32 @@
 package com.chatapp.client;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import com.chatapp.structs.Message;
 import com.private_message.structs.PrivateMessage;
 
-public class ClientPeer {
+public class ClientPeer extends Thread{
     private final String username;
     private final Socket socket;
-    private final ObjectOutputStream objectStream;
+    private final ObjectOutputStream outputStream;
+    private final ObjectInputStream inputStream;
     
 
     public ClientPeer(String username, Socket socket) throws IOException {
         this.username = username;
         this.socket = socket;
-        this.objectStream = new ObjectOutputStream(socket.getOutputStream());
+        this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+        this.inputStream = new ObjectInputStream(socket.getInputStream());
     }
 
     public void sendMessage(String message) {
         try{
             Message _msg = new Message(username, message);
-            objectStream.writeObject(_msg);
-            objectStream.flush();
+            outputStream.writeObject(_msg);
+            outputStream.flush();
         }catch(IOException _ioe){
             System.out.println("Unable to write object: " + _ioe.getMessage());
         }
@@ -32,18 +35,39 @@ public class ClientPeer {
     public void sendMessage(String message, String recipient) {
         try {
             PrivateMessage _pvmsg = new PrivateMessage(recipient, username, message);
-            objectStream.writeObject(_pvmsg);
-            objectStream.flush();
+            outputStream.writeObject(_pvmsg);
+            outputStream.flush();
         } catch (IOException _ioe) {
             System.out.println("Unable to write object: " + _ioe.getMessage());
         }
     }
 
+    @Override
+    public void run() {
+        try {
+            while(true) {
+                Object obj = inputStream.readObject();
+                if(obj instanceof Message _msg) {
+                    System.out.println(_msg);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Connection closed: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Unknown object received: " + e.getMessage());
+        }
+    }
+
     public void close() {
         try {
-            objectStream.close();
+            outputStream.close();
         } catch (IOException _ioe) {
             System.out.println("Unable to close ObjectOutputStream: " + _ioe.getMessage());
+        }
+        try {
+            inputStream.close();
+        } catch (IOException _ioe) {
+            System.out.println("Unable to close ObjectInputStream: " + _ioe.getMessage());
         }
         try {
             socket.close();
@@ -56,7 +80,4 @@ public class ClientPeer {
         return username;
     }
 
-    public Socket getSocket() {
-        return socket;
-    }
 }
